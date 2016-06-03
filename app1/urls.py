f42cfbbc4c24e2f83283d2ae9d026f14125324d6
soheil-arab <soheil.arabzadeh@gmail.com>
2016-06-03 20:00:33 +0430

    Update urls.py

--- app1/urls.py
+++ app1/urls.py
@@ -5,13 +5,13 @@ from . import views
 app_name = 'app1'
 
 urlpatterns = [
-    url(r'match_request$', views.match_request),
-    url(r'update_match_result$', views.update_match_result),
-    url(r'get_updates$', views.get_updates),
-    url(r'update_username$', views.update_username),
-    url(r'get_leaders$', views.get_leaders),
-    url(r'deck$', views.deck),
-    url(r'card$', views.card),
-    url(r'bug_report$', views.bug_report),
-    url(r'bot_deck$', views.bot_deck),
+    url(r'^match_request$', views.match_request),
+    url(r'^update_match_result$', views.update_match_result),
+    url(r'^get_updates$', views.get_updates),
+    url(r'^update_username$', views.update_username),
+    url(r'^get_leaders$', views.get_leaders),
+    url(r'^deck$', views.deck),
+    url(r'^card$', views.card),
+    url(r'^bug_report$', views.bug_report),
+    url(r'^bot_deck$', views.bot_deck),
 ]
