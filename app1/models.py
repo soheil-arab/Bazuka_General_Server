b8aa59aff9cd6a2bbf310c5c24a6c1e19ed1539b
soheil-arab <soheil.arabzadeh@gmail.com>
2016-08-07 13:37:16 +0430

    Update models.py

--- app1/models.py
+++ app1/models.py
@@ -348,8 +348,8 @@ class UserClanData(models.Model):
     lastRequestTime = models.IntegerField(default=0)
     daily_donate_count = models.SmallIntegerField(default=0, null=True, blank=True)
 
-    def __str__(self):
-        return 'ClanData_user#{0}@{1}'.format(self.user.idUser, self.user)
+    # def __str__(self):
+    #     return 'ClanData_user#{0}@{1}'.format(self.user.idUser, self.user)
 
 
 class Donation(models.Model):
